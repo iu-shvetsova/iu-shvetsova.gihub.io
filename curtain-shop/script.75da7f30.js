24eb953699034e82d9f4dc477b8ebd0ceb6a65c8
iu-shvetsova <iu-shvetsova69@yandex.ru>
2020-11-19 10:19:00 +0300

    update

--- curtain-shop/script.75da7f30.js
+++ curtain-shop/script.75da7f30.js
@@ -10760,7 +10760,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65091" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56082" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
